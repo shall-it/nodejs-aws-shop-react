d40e38861a930607d825a5590cfc88c6853fdaa1
Aleksei Shulga <ashulga@transperfect.com>
2023-12-14 19:38:40 +0300

    New REST APIGW

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: "https://h4y1ufebk7.execute-api.us-east-1.amazonaws.com",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://sw4mnq00z4.execute-api.us-east-1.amazonaws.com/prod",
+  import: "https://98tzokb9lf.execute-api.us-east-1.amazonaws.com/prod/",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
