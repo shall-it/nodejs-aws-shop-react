dd23fe6eeedc62a49769d92d3ee4c8bf4e96e8bd
Aleksei Shulga <ashulga@transperfect.com>
2024-01-03 00:45:05 +0300

    Changing of cart from API_PATHS

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -3,7 +3,7 @@ const API_PATHS = {
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   import: "https://98tzokb9lf.execute-api.us-east-1.amazonaws.com/prod",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  cart: "http://shall-it-cart-api-develop.us-east-1.elasticbeanstalk.com",
 };
 
 export default API_PATHS;
